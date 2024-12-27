ada949183d1b04ad01215ed401d7d71236008e74
sinvalfelisberto <sinvalfelisberto@gmail.com>
2024-12-27 10:20:45 -0300

    Commit initial

---
+++ src/main/java/com/felisberto/exceptionhandlers/DemoExceptionHandlersApplication.java
@@ -0,0 +1,13 @@
+package com.felisberto.exceptionhandlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExceptionHandlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExceptionHandlersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felisberto/exceptionhandlers/DemoExceptionHandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felisberto.exceptionhandlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExceptionHandlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
